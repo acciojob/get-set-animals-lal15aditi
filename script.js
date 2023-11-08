c6250192024fd793cafc3e8367ee792dc4184e18
Aditi Lal <139861603+lal15aditi@users.noreply.github.com>
2023-11-08 22:30:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -14,13 +14,13 @@ class Animal {
 
 class Dog extends Animal {
 	bark() {
-    console.log("The Golden Retriever makes a sound");
+    console.log("The Golden Retriever makes a sound woof");
   }
 }
 
 class Cat extends Animal {
 	purr() {
-    console.log("The Siamese makes a sound");
+    console.log("The Siamese makes a sound purr");
   }
 }
 
