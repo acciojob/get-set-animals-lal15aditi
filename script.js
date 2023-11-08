50ab9e9f47844b5a4d5479f2f9f553aa15426b49
Aditi Lal <139861603+lal15aditi@users.noreply.github.com>
2023-11-08 22:32:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,19 +8,19 @@ class Animal {
   }
 
   makeSound() {
-    console.log(`The ${this._species} makes a sound.`);
+    console.log(`The ${this._species} makes a sound`);
   }
 }
 
 class Dog extends Animal {
 	bark() {
-    console.log("The Golden Retriever makes a sound woof");
+    console.log("woof");
   }
 }
 
 class Cat extends Animal {
 	purr() {
-    console.log("The Siamese makes a sound purr");
+    console.log("purr");
   }
 }
 
